5c4c68367788f1b5c7f9bb00117b509dbef8d884
pintotomas <tomasp834@gmail.com>
2021-11-27 12:57:55 -0300

    Replace in-memory db for postgres, dockerize db

--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -13,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Data
+@Table(name="product_order")
 public class Order {
     @Id
     private Long id;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,46 +1,27 @@
-CREATE TABLE digital_product (
-  id bigint PRIMARY KEY,
-  label varchar NOT NULL,
-  type varchar NOT NULL,
-  url varchar NOT NULL
-);
+DROP TABLE IF EXISTS order_product;
+DROP TABLE IF EXISTS cart_product ;
+DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS product_order;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS digital_product;
+DROP TABLE IF EXISTS physical_product;
 
-CREATE TABLE physical_product (
-  "id" bigint PRIMARY KEY,
-  "label" varchar NOT NULL,
-  "type" varchar NOT NULL,
-  "weight" integer NOT NULL
-);
+CREATE TABLE digital_product ("id" BIGINT PRIMARY KEY, "label" varchar NOT NULL, "type" varchar NOT NULL, "url" varchar NOT NULL);
 
-CREATE TABLE order (
-  "id" bigint PRIMARY KEY
-);
+CREATE TABLE physical_product ("id" bigint PRIMARY KEY, "label" varchar NOT NULL, "type" varchar NOT NULL, "weight" integer NOT NULL);
 
-CREATE TABLE cart (
-  "id" bigint PRIMARY KEY,
-  "status" varchar NOT NULL
-);
+CREATE TABLE product_order ("id" bigint PRIMARY KEY);
 
-CREATE TABLE cart_product (
-  "id_product" bigint,
-  "id_cart" bigint,
-  PRIMARY KEY ("id_product", "id_cart")
-);
+CREATE TABLE cart ("id" bigint PRIMARY KEY, "status" varchar NOT NULL);
 
-CREATE TABLE order_product (
-  "id_product" bigint,
-  "id_cart" bigint,
-  PRIMARY KEY ("id_product", "id_cart")
-);
+CREATE TABLE cart_product ("id_product" bigint, "id_cart" bigint, PRIMARY KEY ("id_product", "id_cart"));
 
-CREATE TABLE category (
-  "id" bigint PRIMARY KEY,
-  "label" string NOT NULL,
-  "parent_id" bigint
-);
+CREATE TABLE order_product ("id_product" bigint, "id_cart" bigint, PRIMARY KEY ("id_product", "id_cart"));
+
+CREATE TABLE category ("id" bigint PRIMARY KEY, "label" varchar NOT NULL, "parent_id" bigint);
 
 ALTER TABLE category ADD CONSTRAINT category_parent_id_fk FOREIGN KEY ("parent_id") REFERENCES "category" ("id");
 
 ALTER TABLE cart_product ADD CONSTRAINT cart_product_id_cart_fk FOREIGN KEY ("id_cart") REFERENCES "cart" ("id");
 
-ALTER TABLE order_product ADD CONSTRAINT order_product_id_cart_fk FOREIGN KEY ("id_cart") REFERENCES "order" ("id");
+ALTER TABLE order_product ADD CONSTRAINT order_product_id_cart_fk FOREIGN KEY ("id_cart") REFERENCES "product_order" ("id");
