ad6ccf33724a7955b5e4df73398bd19c276a0e31
pintotomas <tomasp834@gmail.com>
2021-11-28 20:43:15 -0300

    Define and implement service for getting a list of products

--- src/main/java/service/ProductServiceImpl.java
+++ src/main/java/service/ProductServiceImpl.java
@@ -1,8 +1,6 @@
 package service;
 
-import com.online_store.stubs.product.ProductServiceGrpc;
-import com.online_store.stubs.product.ProductRequest;
-import com.online_store.stubs.product.ProductResponse;
+import com.online_store.stubs.product.*;
 import dao.DigitalProductDao;
 import dao.PhysicalProductDao;
 import domain.product.DigitalProduct;
@@ -10,8 +8,7 @@ import domain.product.PhysicalProduct;
 import domain.product.Product;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import com.online_store.stubs.product.Type;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -56,5 +53,34 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
             logger.log(Level.SEVERE, "No product found with id " + productId);
             responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
         }
+    }
+
+    @Override
+    public void getProducts(ProductsRequest request, StreamObserver<ProductsResponse> responseObserver) {
+        List<Long> ids = request.getIdList();
+        ProductsResponse.Builder productsResponse = ProductsResponse.newBuilder();
+        for (Long productId : ids) {
+            Optional<DigitalProduct> optionalDigitalProduct = digitalProductDao.findById(productId);
+            Optional<PhysicalProduct> optionalPhysicalProduct = physicalProductDao.findById(productId);
+            ProductResponse.Builder productResponse = ProductResponse.newBuilder();
+            if (optionalDigitalProduct.isPresent()) {
+                DigitalProduct digitalProduct = optionalDigitalProduct.get();
+                setCommonProductResponse(digitalProduct, productResponse);
+                productResponse.setUrl(digitalProduct.getUrl());
+                productsResponse.addProductResponse(productResponse);
+            }
+            else if (optionalPhysicalProduct.isPresent()) {
+                PhysicalProduct physicalProduct = optionalPhysicalProduct.get();
+                setCommonProductResponse(physicalProduct, productResponse);
+                productResponse.setWeight(physicalProduct.getWeight());
+                productsResponse.addProductResponse(productResponse);
+            }
+            else {
+                logger.log(Level.SEVERE, "No product found with id " + productId);
+                responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+            }
+        }
+        responseObserver.onNext(productsResponse.build());
+        responseObserver.onCompleted();
     }
 }
